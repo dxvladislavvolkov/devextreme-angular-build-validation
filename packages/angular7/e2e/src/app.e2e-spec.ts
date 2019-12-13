cea8c1d6290f605cb78735e54909bbd34044b50b
ovchinnikov.andrey <ovchinnikov@users.noreply.github.com>
2019-12-13 13:46:01 +0300

    ViewChild tests

--- packages/angular7/e2e/src/app.e2e-spec.ts
+++ packages/angular7/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getText()).toEqual('button text\nColumn\ngrid-item');
+    expect(page.getText()).toEqual('button text\nColumn\ngrid-item\nyes');
   });
 
   afterEach(async () => {
--- packages/angular9-ivy-disabled/src/app/app.component.ts
+++ packages/angular9-ivy-disabled/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { DxButtonComponent } from 'devextreme-angular';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular9-ivy-disabled';
+
+  @ViewChild(DxButtonComponent, { static: true })
+  buttonComponent: DxButtonComponent;
 }
